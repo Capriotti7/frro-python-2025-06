e478fc3439beed69a0c82f84273e70e0dabffee6
Leo Carovana <leonelcarovana11@gmail.com>
2025-11-03 22:59:30 -0300

    Corregir el orden de las URLs para arreglar el 404

--- TPI/ispi_oliveros_gestion/gestion_academica/urls.py
+++ TPI/ispi_oliveros_gestion/gestion_academica/urls.py
@@ -1,28 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from web.views import home_view, handle_not_found_view, register_view
-from web.views import crear_superusuario_secreto
+from web.views import home_view, handle_not_found_view, register_view, crear_superusuario_secreto
 
 urlpatterns = [
+    # 1. Rutas principales de la aplicación
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'), # La página pública
+    path('', home_view, name='home'),
 
-    
+    # 2. Rutas de autenticación (Agrupadas)
+    # Primero definimos nuestra ruta de registro personalizada
     path('accounts/register/', register_view, name='register'),
-    # 1. URLs de Autenticación (login, logout, etc.)
-    # Django nos da esto ya hecho.
+    # Luego, incluimos el resto de las URLs de autenticación de Django
     path('accounts/', include('django.contrib.auth.urls')),
 
-    # 2. URLs de nuestra app principal de gestión
+    # 3. Rutas de los módulos de gestión
     path('gestion/', include('core.urls')),
     path('gestion/alumnos/', include('alumnos.urls')),
     path('gestion/academico/', include('academico.urls')),
     path('gestion/finanzas/', include('finanzas.urls')),
 
-    # 3. Manejo de errores 404 con una vista personalizada
-    re_path(r'^.*$', handle_not_found_view, name='catch_all'),
-
-    # --- URL SECRETA PARA CREAR SUPERUSUARIO ---
-    # Usa un nombre largo y difícil de adivinar.
+    # 4. UURL SECRETA PARA CREAR SUPERUSUARIO
     path('crear-primer-superusuario-render-9xyz789/', crear_superusuario_secreto, name='crear_su_secreto'),
+
+    # 5. El "Atrapa-Todo" para 404
+    re_path(r'^.*$', handle_not_found_view, name='catch_all'),
 ]
